b0f7b0128c9a002e3eab9ae0aaf0be3abfb08b00
Brandon Fogel <me@brandonfogel.net>
2021-09-18 14:58:07 -0500

    Changed plugin name and description.  Added Andrew as an author

--- DBNAnalytics.php
+++ DBNAnalytics.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package bfDBN
+ * @package DBNAnalytics
  * @version 1.0
  */
 /*
-Plugin Name: DBN Connect
-Description: This plugin provides access to dynamic content from DBN Dipcast
-Author: Brandon Fogel
+Plugin Name: DBN Analytics
+Description: This plugin provides access to dynamic content related to DBN broadcasts and analytical tools for use with the game of Diplomacy 
+Author: Brandon Fogel, Andrew Zick
 Version: 1.0
 */
 
@@ -19,5 +19,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 include("modules/system.php");
 include("modules/competition.php");
 include("modules/player.php");
+include("modules/scoringcalculator.php");
 
 ?>
