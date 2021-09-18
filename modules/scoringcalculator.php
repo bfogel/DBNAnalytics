40fd07ce8264e9f5a5d76f2581aac8e4a0db5b15
Brandon Fogel <me@brandonfogel.net>
2021-09-18 15:27:21 -0500

    links take href, not src

--- modules/scoringcalculator.php
+++ modules/scoringcalculator.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_shortcode( 'dbnScoringCalculator', 'dbnScoringCalculator_Create' );
 
 function dbnScoringCalculator_Create() {
-    $ret = "<link rel = 'stylesheet' src = 'https://diplobn.com/wp-content/plugins/DBNAnalytics/css/ScoringCalculator.css'>";
+    $ret = "<link rel = 'stylesheet' href = 'https://diplobn.com/wp-content/plugins/DBNAnalytics/css/ScoringCalculator.css'>";
     $ret .= "<script src = 'https://diplobn.com/wp-content/plugins/DBNAnalytics/js/ScoringFunctions.js'></script>";
     $ret .= "<script src = 'https://diplobn.com/wp-content/plugins/DBNAnalytics/js/ScoringCalculator.js'></script>";
     $ret .= "<div id = 'ScoringCalculator'></div>";
