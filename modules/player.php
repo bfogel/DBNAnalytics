11b9dd69bac3411fffa900769de1e9db1bc7033d
Brandon Fogel <me@brandonfogel.net>
2022-02-02 13:03:10 -0500

    Debugging css issue

--- modules/player.php
+++ modules/player.php
@@ -16,7 +16,7 @@ function dbnPlayer_MainPage() {
     return $ret;
 }
 
-function dbnVersion(){return 6;}
+function dbnVersion(){return 7;}
 
 add_shortcode( 'dbnPlayerVsPlayer', 'dbnPlayerVsPlayer_Create' );
 function dbnPlayerVsPlayer_Create() {
