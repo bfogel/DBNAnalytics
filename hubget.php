75de390db199d0d29f6a30384a21ab0edfd08f65
Brandon Fogel <me@brandonfogel.net>
2022-02-07 09:25:56 -0500

    hubget refactor

--- hubget.php
+++ hubget.php
@@ -27,9 +27,9 @@ function HandleRequest($request)
     switch ($request["Key"]) {
         case 'profiles':
             $parms = $request["Parameters"];
-            return ["sql" => 'SELECT PlayerID, PlayerName FROM Player WHERE Token = "' . $parms["token"] + '"'];
+            // return ["sql" => 'SELECT PlayerID, PlayerName FROM Player WHERE Token = "' . $parms["token"] . '"'];
             //REMOVE DECODE HERE ONCE GETANDRETURN IS REFACTORED
-            return json_decode(GetAndReturnJSON('SELECT PlayerID, PlayerName FROM Player WHERE Token = "' . $parms["token"] + '"'));
+            return json_decode(GetAndReturnJSON('SELECT PlayerID, PlayerName FROM Player WHERE Token = "' . $parms["token"] . '"'));
         default:
             return "wut";
     }
