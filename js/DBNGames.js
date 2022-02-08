eca728254a782e5d2fe039d795772759f7d9b708
Brandon Fogel <me@brandonfogel.net>
2022-02-07 22:27:03 -0500

    hubget

--- js/DBNGames.js
+++ js/DBNGames.js
@@ -134,7 +134,7 @@ class dbnHub {
     var vals = { "p1": player1id };
     if (player2id != null) vals["p2"] = player2id;
 
-    var req = new dbnHubRequest("games2", vals);
+    var req = new dbnHubRequest("games", vals);
     req.SendAlone();
 
     console.log(req.ResponseContent);
