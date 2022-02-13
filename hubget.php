dee2efeea457d3c863bc6ab8e82aa3b655b3169a
Brandon Fogel <me@brandonfogel.net>
2022-02-13 16:50:58 -0500

    glurg

--- hubget.php
+++ hubget.php
@@ -11,8 +11,8 @@ include("/home/customer/www/diplobn.com/public_html/wp-includes/class-wp-user.ph
 //still trying
 include("/home/customer/www/diplobn.com/public_html/wp-includes/option.php");
 include("/home/customer/www/diplobn.com/public_html/wp-includes/load.php");
-include("/home/customer/www/diplobn.com/public_html/wp-includes/class-wp-object-cache.php");
 include("/home/customer/www/diplobn.com/public_html/wp-includes/cache.php");
+include("/home/customer/www/diplobn.com/public_html/wp-includes/class-wp-object-cache.php");
 
 
 header("Access-Control-Allow-Origin: *");
