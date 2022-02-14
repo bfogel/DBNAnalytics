ae58941c7cfdd2ea1a5d76ecccd4fa303d3e8290
Brandon Fogel <me@brandonfogel.net>
2022-02-14 08:31:01 -0500

    trying

--- hubget.php
+++ hubget.php
@@ -41,7 +41,7 @@ function hubget_respond($data)
 {
     $myrequests = $_POST['requests'];
 
-    return json_encode($myrequests);
+    return json_decode($myrequests);
 
     if ($myrequests != "") {
         $list = json_decode($myrequests, true);
