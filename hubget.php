383e74702c1f411f70b2b5e5ff5d6bad1078a421
Brandon Fogel <me@brandonfogel.net>
2022-02-14 08:47:59 -0500

    hubget

--- hubget.php
+++ hubget.php
@@ -33,7 +33,7 @@ add_action('rest_api_init', function () {
     remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
 
     add_filter('rest_pre_serve_request', function ($value) {
-        //header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: *");
     });
 }, 15);
 
