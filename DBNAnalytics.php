30b3fc3ac5a9d59a935ba48d5334dfde2e88cfc2
Brandon Fogel <me@brandonfogel.net>
2022-02-18 08:32:38 -0500

    user redirect

--- DBNAnalytics.php
+++ DBNAnalytics.php
@@ -32,4 +32,23 @@ function disable_owp_dashboard_news_widget() {
 	remove_meta_box( 'owp_dashboard_news', 'dashboard', 'normal');
 }
 add_action('wp_dashboard_setup', 'disable_owp_dashboard_news_widget', 40);
+
+function redirect_admin( $redirect_to, $request, $user ){
+
+    //is there a user to check?
+
+    if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+
+        //check for admins
+        if ( in_array( 'administrator', $user->roles ) ) {
+
+            $redirect_to = '/player-portal/'; // Your redirect URL
+        }
+    }
+
+    return $redirect_to;
+}
+
+add_filter( 'login_redirect', 'redirect_admin', 10, 3 );
+
 ?>
