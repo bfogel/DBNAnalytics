c57cd268fc5d84b97659eedcd4ce739207711c19
Brandon Fogel <me@brandonfogel.net>
2022-03-28 19:03:51 -0400

    version

--- modules/player.php
+++ modules/player.php
@@ -22,7 +22,7 @@ function dbnPlayer_MainPage()
 class dbnResponder
 {
 
-    public $Version = 46;
+    public $Version = 47;
 
     public $CSS_DBNAnalytics = true;
     public $CSS_PowerAuction = false;
