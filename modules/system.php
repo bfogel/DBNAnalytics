74ba68b28a65edacdc58bff7c8f70aa8b43a4c97
Brandon Fogel <me@brandonfogel.net>
2022-07-13 19:18:14 -0500

    version

--- modules/system.php
+++ modules/system.php
@@ -32,7 +32,7 @@ function dbn_GetHTMLWithConn($conn, $category, $id) {
 class dbnResponder
 {
 
-    public $Version = 62;
+    public $Version = 63;
 
     public $CSS_DBNAnalytics = true;
     public $CSS_PowerAuction = false;
