d67a94559ccba7108fa40ff912df3b1a56e91584
Brandon Fogel <me@brandonfogel.net>
2022-07-15 17:36:41 -0500

    shortcode debug

--- modules/competition.php
+++ modules/competition.php
@@ -48,8 +48,6 @@ add_shortcode('dbnCompetitionGroupPage', 'dbnCompetitionGroup_GetPage');
 function dbnCompetitionGroup_GetPage($atts = [], $content = null, $tag = '')
 {
 	$responder = new dbnResponder();
-	return json_encode($atts);
-
 	$responder->HubParameters = $atts;
 	$responder->JS_CompetitionGroupPage = true;
 	return $responder->Generate();
